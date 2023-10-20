7ac1ff9a4b6fa192db49c8c8dbb62dcfe5ea43d0
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-20 18:04:04 +0530

    added method for create payment

--- src/main/java/com/devtechnexus/paymentservice/service/PaymentService.java
+++ src/main/java/com/devtechnexus/paymentservice/service/PaymentService.java
@@ -1,8 +1,15 @@
 package com.devtechnexus.paymentservice.service;
 
+import com.paypal.api.payments.*;
 import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PaymentService {
@@ -10,5 +17,41 @@ public class PaymentService {
     @Autowired
     private APIContext apiContext;
 
-    public Payment
+    public Payment createPayment(Double total,
+                                 String currency,
+                                 String method,
+                                 String intent,
+                                 String description,
+                                 String cancelUrl,
+                                 String successUrl) throws PayPalRESTException {
+
+        Amount amount = new Amount();
+        amount.setCurrency(currency);
+        double AccurateTotal = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
+        amount.setTotal(String.format("%.2f", AccurateTotal));
+
+        Transaction transaction = new Transaction();
+        transaction.setDescription(description);
+        transaction.setAmount(amount);
+
+        List<Transaction> transactionList = new ArrayList<>();
+        transactionList.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod(method.toString());
+
+        Payment payment = new Payment();
+        payment.setIntent(intent.toString());
+        payment.setPayer(payer);
+        payment.setTransactions(transactionList);
+
+        RedirectUrls urls = new RedirectUrls();
+        urls.setCancelUrl(cancelUrl);
+        urls.setReturnUrl(successUrl);
+        payment.setRedirectUrls(urls);
+
+        return payment.create(apiContext);
+    }
+
+
 }
