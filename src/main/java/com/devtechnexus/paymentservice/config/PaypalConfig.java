0a7e8f9bd5b1ff62db57897433198371cccebfd0
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-20 17:12:48 +0530

    added beans in paypalconfig

--- src/main/java/com/devtechnexus/paymentservice/config/PaypalConfig.java
+++ src/main/java/com/devtechnexus/paymentservice/config/PaypalConfig.java
@@ -1,7 +1,13 @@
 package com.devtechnexus.paymentservice.config;
 
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 /**
  * PaypalConfig
@@ -24,4 +30,24 @@ public class PaypalConfig {
 
     @Value("${PAYPAL_MODE}")
     private String mode;
+
+    @Bean
+    public Map<String, String> paypalSdkConfig() {
+        return Map.of(
+                "mode", mode,
+                "clientId", clientId,
+                "clientSecret", clientSecret
+        );
+    }
+
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential() {
+        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+    }
+
+    public APIContext apiContext() throws PayPalRESTException {
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
+    }
 }
