4b6a5ebc4fc0108d95a18d6bad4cf3a20ca400b4
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-20 17:08:23 +0530

    adding docs for PaypalConfig

--- src/main/java/com/devtechnexus/paymentservice/config/PaypalConfig.java
+++ src/main/java/com/devtechnexus/paymentservice/config/PaypalConfig.java
@@ -3,6 +3,16 @@ package com.devtechnexus.paymentservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * PaypalConfig
+ * Configuration Bean for PayPal API.
+ * <br><br>
+ * ALL VALUES ARE STORED IN SECRETS.PROPERTIES FILE.
+ * SECRETS.PROPERTIES IS NOT COMMITTED NOR SHARED AS IT CONTAINS SENSITIVE INFORMATION.
+ * <br>
+ * check out the ReadME.md for more info.
+ *
+ */
 @Configuration
 public class PaypalConfig {
 
@@ -12,4 +22,6 @@ public class PaypalConfig {
     @Value("${PAYPAL_CLIENT_SECRET}")
     private String clientSecret;
 
+    @Value("${PAYPAL_MODE}")
+    private String mode;
 }
